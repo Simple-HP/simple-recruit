91fd850914ec712c1cd5d49b701f26fab0d512d8
shinnkura <n.shinzo1010@icloud.com>
2025-05-13 11:15:34 +0900

    add:export const dynamic = "force-static";

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = "force-static";
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
